d8c37057a9586e4318e37d5dcae30ca94a9a98e3
jihanv <hedjmny@gmail.com>
2025-08-14 21:26:56 +0900

    Implement search form

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,10 @@
+"use client"
+
 import Link from "next/link";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion"
 
 const routes = [
     {
@@ -13,17 +18,25 @@ const routes = [
 ]
 
 export default function Header() {
+
+    const activePathName = usePathname();
+    console.log(activePathName)
     return (
         <header className="flex justify-between 
         items-center border-b border-white/10 h-14 px-3 sm:px-9 ">
             <Logo />
-            <nav>
-                <ul className="flex gap-x-6 text-sm">
+            <nav className="h-full">
+                <ul className="flex gap-x-6 h-full text-sm">
                     {
                         routes.map((route) => (
                             <li key={route.path}
-                                className="text=white/50 hover:text-white transition">
+                                className={clsx(" hover:text-white items-center relative transition", {
+                                    "text-white": activePathName === route.path,
+                                    "text-white/50": activePathName !== route.path,
+                                })}>
                                 <Link href={route.path}>{route.name}</Link>
+                                {activePathName === route.path && <motion.div layoutId="header-active-link" className="bg-accent h-1 w-full absolute bottom-0"></motion.div>}
+
                             </li>
                         ))
                     }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import Link from "next/link";
 import { ACCENT_COLOR } from "./lib/constants";
+import SearchForm from "./components/Search-form";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-3 pt-36">
       <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">Find events around you</h1>
-      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className={`font-bold italic underline text-[${ACCENT_COLOR}]`}>10,000 events</span> around you</p>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">Browse more than <span className={`font-bold italic underline text-accent`}>10,000 events</span> around you</p>
 
-      <form className={`w-full sm:w-[580px]`}>
-        <input className={`w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[${ACCENT_COLOR}]/50 transition focus:ring-2 focus:bg-white/10`} placeholder="Search events in any city ..."
-          spellCheck={false} />
-      </form>
+      <SearchForm />
 
       <section className="mt-4 flex gap-x-4 ttext-sm text-white/50">
         <p>Popular:</p>
