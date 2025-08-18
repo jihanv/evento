44a383251e91fa56914e5d22d0464822e25c6dbf
jihanv <hedjmny@gmail.com>
2025-08-18 10:15:58 +0900

    Replace Fetch API with Prisma Client

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { EventoEvent } from "@prisma/client";
+import { EventoEvent, PrismaClient } from "@prisma/client";
+import prisma from "./db";
+
+// const prisma = new PrismaClient();
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(...inputs));
@@ -11,20 +14,20 @@ export function capitalize(text: string) {
 }
 
 export async function getEvents(city: string) {
-  const result = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
-  const events: EventoEvent[] = await result.json();
+  const events = await prisma.eventoEvent.findMany({
+    where: {
+      city: city === "all" ? undefined : capitalize(city),
+    },
+  });
 
   return events;
 }
 
 export async function getEvent(slug: string) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-
-  const event: EventoEvent = await response.json();
-
+  const event = await prisma.eventoEvent.findUnique({
+    where: {
+      slug: slug,
+    },
+  });
   return event;
 }
